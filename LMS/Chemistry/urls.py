3780c396cd694638f1e9b7ec6f10ead1c12be72e
dinesh <133356394+dinonomous@users.noreply.github.com>
2023-12-20 11:18:50 +0530

    fixing spelling mistakes

--- LMS/Chemistry/urls.py
+++ LMS/Chemistry/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from Chemistory import views
+from Chemistry import views
 
 urlpatterns = [
     path('',views.unit1,name="cheunit1"),
